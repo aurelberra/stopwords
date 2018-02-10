d01c10586291c695810da93e0568956d20e7001d
Aurélien Berra <aurelien.berra@gmail.com>
2018-02-10 10:13:41 +0100

    make versions

--- make_lists.R
+++ make_lists.R
@@ -8,7 +8,7 @@ source("~/Documents/github/r-dev/helpers.R")
 # GREEK
 
 # Set version number
-version_greek <- "2.2"
+version_greek <- "2.3"
 
 # Convert current JSON list to TXT with Markdown headings
 greek_json <- read_file("stopwords_greek.json")
@@ -59,7 +59,7 @@ write_file(stopwords_greek, "stopwords_greek.txt")
 # LATIN
 
 # Set version number
-version_latin <- "2.1"
+version_latin <- "2.2"
 
 # Convert current JSON list to TXT with Markdown headings
 latin_json <- read_file("stopwords_latin.json")
